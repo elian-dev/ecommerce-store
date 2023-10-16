bfea82bf15a9e5d2004bbc93398b691755523b59
elian-dev <geordymemdoza@gmail.com>
2023-10-16 01:03:48 -0600

    feat: :sparkles: Add to cart function in PDP

--- components/gallery/info.tsx
+++ components/gallery/info.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import { MouseEventHandler } from "react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
     data: Product
@@ -10,6 +14,13 @@ interface InfoProps {
 const Info: React.FC<InfoProps> = ({
     data
 }) => {
+    const cart = useCart();
+
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+        cart.addItem(data)
+    }
+    
     return ( 
         <div>
             <h1 className="text-3xl font-bold text-gray-900">
@@ -39,7 +50,7 @@ const Info: React.FC<InfoProps> = ({
             </div>
 
             <div className="mt-10 flex items-center gap-x-3">
-                <Button className="flex items-center gap-x-2">
+                <Button onClick={onAddToCart} className="flex items-center gap-x-2">
                     Add to cart
                     <ShoppingCart />
                 </Button>
